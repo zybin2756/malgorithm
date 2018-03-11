d43b5835f1c85fd06c122ad9fed9bd43f0feee78
zybin2756 <zybns@qq.com>
2018-03-11 22:06:08 +0800

    新增某区间插入排序,优化归并

--- main.cpp
+++ main.cpp
@@ -11,19 +11,16 @@ using namespace std;
 int main()
 {
     int n = 10;
-    while(n <= 100000000){
+    while(n < 1000000000){
     
         int* arr1 = SortTestHelper::createRandomArray(1,n,n);
 //        int* arr1 = SortTestHelper::crateNealySortArray(n, 10);
 //        int* arr2 = SortTestHelper::copyArray(arr1, n);
 //        int* arr3 = SortTestHelper::copyArray(arr1, n);
 //        int* arr4 = SortTestHelper::copyArray(arr1, n);
-//        SortTestHelper::printArray(arr1, n);
         SortTestHelper::sortTest("mergeSortUD", mergeSortUD, arr1, n);
 //        SortTestHelper::sortTest("Sedgewick ", shellSort1, arr2, n);
-//        SortTestHelper::sortTest("Hibbard ", shellSort2, arr3, n);
-//        SortTestHelper::sortTest("Knuth ", shellSort3, arr4, n);
-//        SortTestHelper::printArray(arr1, n);
+
         cout << "--------------------------------------" << endl;
         delete[] arr1;
 //        delete[] arr2;
--- sort/mergesort.h
+++ sort/mergesort.h
@@ -1,11 +1,25 @@
 #ifndef MERGESORT_H
 #define MERGESORT_H
 
+//插入排序 排序[l..r]区间
+template<typename T>
+void insertSort(T arr[], int l, int r){
+    int j;
+    for(int i =l+1; i <= r; ++i){
+        T val = arr[i];
+        for(j = i; j > l && val < arr[j-1]; --j){
+            arr[j] = arr[j-1];
+        }
+        arr[j] = val;
+    }
+}
+
 
 //合并[l..m-1] 和　[m..r]　为有序区间
 template<typename T>
 void __merge(T arr[], int l, int m, int r){
     T* aux = new T[r-l+1];//申请辅助空间，这里前闭后闭的区间所以要+1，如果是其他的请酌情考虑
+//    T aux[r-l+1];
     for(int i = l; i <= r; ++i){
         aux[i-l] = arr[i];
     }
@@ -35,13 +49,15 @@ void __merge(T arr[], int l, int m, int r){
 //排序[l..r]的前闭后闭区间
 template<typename T>
 void __mergeSortUD(T arr[], int l, int r){
-    if(l >= r){
+    if(r-l <= 30){
+        insertSort(arr, l, r);
         return;
     }
     int mid = (r-l)/2 + l; //避免越界
     __mergeSortUD(arr, l, mid);
     __mergeSortUD(arr, mid+1, r);
-    __merge(arr, l, mid, r);
+    if(arr[mid] > arr[mid+1])
+        __merge(arr, l, mid, r);
 }
 
 template<typename T>
