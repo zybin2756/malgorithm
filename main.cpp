1522bad161bc989bb67e78c35422c2bd92642b9c
zyb <zybns@qq.com>
2018-03-14 16:54:18 +0800

    三路快速排序

--- main.cpp
+++ main.cpp
@@ -7,30 +7,35 @@
 #include "sort/mergesort.h"
 #include "sort/quicksort.h"
 
+#include "question.h"
+
 using namespace std;
 
 int main()
 {
     int n = 10;
-    while(n < 100000000){ //
+    cout << "----------test random----------:" << endl;
+    while(n < 100){ //
     
         int* arr1 = SortTestHelper::createRandomArray(1,n,n);
-        int* arr2 = SortTestHelper::crateNealySortArray(n, 10);
-        int* arr3 = SortTestHelper::createRandomArray(1,10,n);
-//        int* arr2 = SortTestHelper::copyArray(arr1, n);
+//        int* arr2 = SortTestHelper::crateNealySortArray(n, 10);
+//        int* arr3 = SortTestHelper::createRandomArray(1,10,n);
+        int* arr2 = SortTestHelper::copyArray(arr1, n);
 //        int* arr3 = SortTestHelper::copyArray(arr1, n);
 //        int* arr4 = SortTestHelper::copyArray(arr1, n);
-//        SortTestHelper::printArray(arr1, n);
-        SortTestHelper::sortTest("quicksort random", quicksort, arr1, n);
-        SortTestHelper::sortTest("quicksort nearlysorted ", quicksort, arr2, n);
-        SortTestHelper::sortTest("quicksort much repeated ", quicksort, arr3, n);
-//        SortTestHelper::printArray(arr1, n);
-        cout << "--------------------------------------" << endl;
+
+        SortTestHelper::sortTest("quicksort2ways", quicksort, arr1, n);
+        SortTestHelper::sortTest("quicksort3ways", quicksort3ways, arr2, n);
+//        SortTestHelper::sortTest("mergeSortUD", mergeSortUD, arr3, n);
+//
+//        cout << "--------------------------------------" << endl;
         delete[] arr1;
         delete[] arr2;
-        delete[] arr3;
+//        delete[] arr3;
 //        delete[] arr4;
         n*=10;
+        cout << "--------------------------------------" << endl;
     }
+
     return 0;
 }
